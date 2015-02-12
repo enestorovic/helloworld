b826be7f2c704e7c55cec9fec01a169bd9e74846
enestorovic <enestorovic@gmail.com>
2015-02-12 15:21:40 -0500

    capistrano deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,96 +1,78 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-# set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
-
-
-
-
 # Define the name of the application
+require 'bundler/capistrano'
 set :application, 'helloworld'
 
 # Define where can Capistrano access the source repository
 # set :repo_url, 'https://github.com/[user name]/[application name].git'
 set :scm, :git
-set :repo_url, 'https://github.com/user123/my_app.git'
+set :repo_url, 'https://github.com/enestorovic/helloworld.git'
+set :branch, "master"
+set :shallow_clone, 1
 
 # Define where to put your application code
-set :deploy_to, "/home/deployer/apps/my_app"
+set :deploy_to, "/var/www/helloworld"
+
+set :user, "deploy" #this is the ubuntu user we created
+set :password, "ebilu529" #deploy's password
+set :use_sudo, false
+
+set :mysql_user, "deploy" #this is the mysql user we created
+set :mysql_password, "secret"
 
 set :pty, true
 
 set :format, :pretty
 
-# Set the post-deployment instructions here.
-# Once the deployment is complete, Capistrano
-# will begin performing them as described.
-# To learn more about creating tasks,
-# check out:
-# http://capistranorb.com/
 
-# namespace: deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :publishing, :restart
-
+# namespace :deploy do
+#
 #   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 d
 #       # Here we can do anything such as:
 #       # within release_path do
 #       #   execute :rake, 'cache:clear'
 #       # end
 #     end
 #   end
-
+#
 # end
+
+
+#Passenger
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+
+
+after "deploy:setup", "db_yml:create"
+after "deploy:update_code", "db_yml:symlink"
+
+namespace :db_yml do
+  desc "Create database.yml in shared path" 
+  task :create do
+    config = {
+              "production" => 
+              {
+                "adapter" => "mysql2",
+                "socket" => "/var/run/mysqld/mysqld.sock",
+                "username" => mysql_user,
+                "password" => mysql_password,
+                "database" => "#{application}_production"
+              }
+            }
+    put config.to_yaml, "#{shared_path}/database.yml"
+  end
+
+  desc "Make symlink for database.yml" 
+  task :symlink do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml" 
+  end
+end
